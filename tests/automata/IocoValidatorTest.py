29fddecc5bdf8e9c49e14360da658eb8f629e24c
Rene Haubitzer <haubitzer@student.tugraz.at>
2021-09-01 10:40:05 +0200

    added test for ioco

---
+++ tests/automata/IocoValidatorTest.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+from aalpy.automata import IotsMachine, IocoValidator
+from aalpy.utils import load_automaton_from_file
+
+test_cases = [
+    ("../../DotModels/Iots/ioco/01_ioco_S.dot", "../../DotModels/Iots/ioco/01_ioco_P.dot", True),
+    ("../../DotModels/Iots/ioco/02_ioco_S.dot", "../../DotModels/Iots/ioco/02_ioco_P.dot", True),
+    ("../../DotModels/Iots/ioco/03_ioco_S.dot", "../../DotModels/Iots/ioco/03_ioco_P.dot", False),
+    ("../../DotModels/Iots/ioco/04_ioco_S.dot", "../../DotModels/Iots/ioco/04_ioco_P.dot", True),
+    ("../../DotModels/Iots/ioco/05_ioco_S.dot", "../../DotModels/Iots/ioco/05_ioco_P.dot", True),
+    ("../../DotModels/Iots/ioco/06_ioco_S.dot", "../../DotModels/Iots/ioco/06_ioco_P.dot", False),
+]
+
+
+class IocoValidatorTest(TestCase):
+
+    def test_ioco_check(self):
+        for spec_path, impl_path, expect in test_cases:
+            with self.subTest(msg="Checking if ioco relation between:", S=spec_path, P=impl_path, E=expect):
+                specification: IotsMachine = load_automaton_from_file(spec_path, "iots")
+                implementation: IotsMachine = load_automaton_from_file(impl_path, "iots")
+                actual = IocoValidator(specification).check(implementation)
+
+                self.assertEqual(actual, expect)
